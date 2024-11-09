5d00fd5ecf13c76c66ac67450c4bba46ef5694ca
joseph <josehp304@gmail.com>
2024-11-09 12:05:22 +0530

    updated map

--- src/footer.js
+++ src/footer.js
@@ -35,7 +35,7 @@ export default function Footer() {
           }}
         >
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1232.6624450321287!2d76.47911696962109!3d9.74642972114493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b07d640a928816d%3A0x4bb20fe5391b84ad!2sKunnumpurathu%20Home%20Gallery!5e1!3m2!1sen!2sin!4v1723119294806!5m2!1sen!2sin"
+            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3925.82021742843!2d76.20832787503484!3d10.276065289843899!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2sin!4v1731133907410!5m2!1sen!2sin"
             width="300"
             height="300"
             style={{ border: "0" }}
